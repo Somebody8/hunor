e3e8bb597d5bb994a5d3cdfb6f1d505676892831
Somebody8 <73525702+Somebody8@users.noreply.github.com>
2024-02-10 23:11:52 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -10,8 +10,4 @@ st.markdown("[Nagyon jó dal](https://youtu.be/e_04ZrNroTo?si=HWN2Lo5OdquWAAxL)"
 st.markdown(">Valami")
 st.markdown("---")
 st.caption("Valaki vagyok")
-st.latex(\sum_{
-\begin{subarray}{l}
-   i\in\Lambda\\
-   0<j<n
-\end{subarray}})
+st.latex(a')
